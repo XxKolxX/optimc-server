4090d870d4694e4674f65b1de95bf374f0771db0
XxKolxX <paterybra122@gmail.com>
2026-06-16 22:16:28 +0200

    Add files via upload

--- web/app.js
+++ web/app.js
@@ -1877,8 +1877,7 @@ function interpolateSinglePlayer(p, role = 'local') {
     if (!interpolatedPlayers[p.uuid]) {
         interpolatedPlayers[p.uuid] = { x: p.x, z: p.z, yaw: p.yaw };
     } else {
-        const ip = interpolatedPlayers[p.uuid];
-        const amt = (role === 'local') ? 0.15 : 0.04;
+        const amt = (role === 'local') ? 0.35 : 0.2;
         ip.x = lerp(ip.x, p.x, amt);
         ip.z = lerp(ip.z, p.z, amt);
         
